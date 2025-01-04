08a41b8f4842799df68dc01d5bac65469fb018a0
Tanjuate Achaleke <tanjuateachaleke@gmail.com>
2025-01-04 14:08:14 +0100

    Added all the Crude operations for the users from getting all users, to geting a single user, to adding a new user, etc

--- src/main/java/com/brunostar/csc/service/UserService.java
+++ src/main/java/com/brunostar/csc/service/UserService.java
@@ -1,15 +1,48 @@
 package com.brunostar.csc.service;
 
+import com.brunostar.csc.dto.UserDTO;
 import com.brunostar.csc.model.User;
 import com.brunostar.csc.repository.UserRepository;
+import com.brunostar.csc.util.UserNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.sql.Timestamp;
+import java.util.List;
+
+@Service
 public class UserService {
 
     @Autowired
     private UserRepository userRepository;
 
     public User createUser(User user) {
-        return user;
+        user.setCreated_at(new Timestamp(System.currentTimeMillis()));
+        return userRepository.save(user);
+    }
+
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUserById(long userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(() -> new UserNotFoundException("User not found with id: " + userId));
+    }
+
+    public void deleteUser(Long userId) throws UserNotFoundException {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new UserNotFoundException("User not found with id: " + userId));
+        userRepository.delete(user);
+    }
+
+    public User updateUser(User existingUser, UserDTO userDTO) {
+        if (userDTO.getEmail() != null) {
+            existingUser.setEmail(userDTO.getEmail());
+        }
+        if (userDTO.getUsername() != null) {
+            existingUser.setUsername(userDTO.getUsername());
+        }
+        return userRepository.save(existingUser);
     }
 }
