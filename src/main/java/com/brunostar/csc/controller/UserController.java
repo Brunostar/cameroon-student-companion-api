37a8e069687de4441394923e2a32216d1faf1df1
Tanjuate Achaleke <tanjuateachaleke@gmail.com>
2025-01-04 10:40:47 +0100

    Created a User controller This will control or requests made to the user api

---
+++ src/main/java/com/brunostar/csc/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.brunostar.csc.controller;
+
+import com.brunostar.csc.model.User;
+import com.brunostar.csc.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api/users/")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.createUser(user);
+    }
+
+}
