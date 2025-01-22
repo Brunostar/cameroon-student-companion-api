e40b99d116f22ad1efd19b1e0f630d263c5c6ef7
Tanjuate Achaleke <tanjuateachaleke@gmail.com>
2025-01-22 03:06:35 +0100

    Made changes to the code for the user api this code includes the user class

--- src/main/java/com/brunostar/csc/model/User.java
+++ src/main/java/com/brunostar/csc/model/User.java
@@ -19,4 +19,73 @@ public class User {
     private String username;
     private Timestamp created_at;
     private Timestamp updated_at;
+
+    public User(long id) {
+        this.user_id = id;
+    }
+
+    public User() {
+    }
+
+    @PrePersist
+    protected void onCreate() {
+        created_at = updated_at = new Timestamp(System.currentTimeMillis());
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updated_at = new Timestamp(System.currentTimeMillis());
+    }
+
+    public User(Long id) {
+        this.user_id = id;
+    }
+
+    public long getUser_id() {
+        return user_id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public Timestamp getCreated_at() {
+        return created_at;
+    }
+
+    public Timestamp getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUser_id(long user_id) {
+        this.user_id = user_id;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setCreated_at(Timestamp created_at) {
+        this.created_at = created_at;
+    }
+
+    public void setUpdated_at(Timestamp updated_at) {
+        this.updated_at = updated_at;
+    }
 }
